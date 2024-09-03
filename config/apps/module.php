001c7cef93f9232d2a2f6127a1d224cf4957eb7e
nhathuy <lamnhathuy0393418721@gmail.com>
2024-09-03 16:05:27 +0700

    hotfix: chỉnh sửa module

--- config/apps/module.php
+++ config/apps/module.php
@@ -23,9 +23,23 @@ return [
         ],
         [
             'user_role' => [],
+            'title' => 'Người Dùng',
+            'icon' => 'fa fa-user',
+            'route' => ['user.index'],
+            'subModule' => [
+                [
+                    'title' => 'Danh Sách',
+                    'route' => 'user.index',
+                    'icon' => 'fa fa-list',
+                    'user_role' => []
+                ],
+            ]
+        ],
+        [
+            'user_role' => [],
             'title' => 'Kho',
             'icon' => 'fa fa-warehouse',
-            'route' => ['warehouse.import', 'warehouse.export','warehouse.inventory'],
+            'route' => ['warehouse.import', 'warehouse.export', 'check_warehouse.index', 'card_warehouse.index'],
             'subModule' => [
                 [
                     'title' => 'Nhập Kho',
@@ -40,11 +54,91 @@ return [
                     'user_role' => []
                 ],
                 [
-                    'title' => 'Tồn Kho',
-                    'route' => 'warehouse.inventory',
-                    'icon' => 'fas fa-clipboard-list',
+                    'title' => 'Kiểm Kho',
+                    'route' => 'check_warehouse.index',
+                    'icon' => 'fa fa-archive',
+                    'user_role' => []
+                ],
+                [
+                    'title' => 'Thẻ Kho',
+                    'route' => 'card_warehouse.index',
+                    'icon' => 'fa fa-clipboard',
                     'user_role' => []
                 ]
+            ]
+        ],
+        [
+            'user_role' => [],
+            'title' => 'Vật Tư',
+            'icon' => 'fa-solid fa-suitcase-medical',
+            'route' => ['material.index', 'material.material_group'],
+            'subModule' => [
+                [
+                    'title' => 'Danh Sách Vật Tư',
+                    'route' => 'material.index',
+                    'icon' => 'fa fa-pump-medical',
+                    'user_role' => []
+                ],
+                [
+                    'title' => 'Danh Sách Nhóm Vật Tư',
+                    'route' => 'material.material_group',
+                    'icon' => 'fa fa-notes-medical',
+                    'user_role' => []
+                ]
+            ]
+        ],
+        [
+            'user_role' => [],
+            'title' => 'Yêu Cầu Đặt Hàng',
+            'icon' => 'fa fa-bell-concierge',
+            'route' => ['order_request.index'],
+            'subModule' => [
+                [
+                    'title' => 'Danh Sách Yêu Cầu',
+                    'route' => 'order_request.index',
+                    'icon' => 'fa fa-rectangle-list',
+                    'user_role' => []
+                ],
+            ]
+        ],
+        [
+            'user_role' => [],
+            'title' => 'Thông Báo',
+            'icon' => 'fa fa-bell',
+            'route' => ['notification.index', 'notification.notification_type'],
+            'subModule' => [
+                [
+                    'title' => 'Danh Sách Thông Báo',
+                    'route' => 'notification.index',
+                    'icon' => 'fa fa-bell',
+                    'user_role' => []
+                ],
+                [
+                    'title' => 'Loại Thông Báo',
+                    'route' => 'notification.notification_type',
+                    'icon' => 'fa fa-list',
+                    'user_role' => []
+                ],
+            ],
+            [
+                'title' => 'Tồn Kho',
+                'route' => 'warehouse.inventory',
+                'icon' => 'fas fa-clipboard-list',
+                'user_role' => []
+            ]
+        ],
+        [
+            'user_role' => [],
+            'title' => 'Nhà Cung Cấp',
+            'icon' => 'fa fa-truck',
+            'route' => ['supplier.list'],
+            'subModule' => [
+                [
+                    'title' => 'Nhà cung cấp ',
+                    'route' => 'supplier.list',
+                    'icon' => 'fa fa-address-book',
+                    'user_role' => []
+                ],
             ]
         ],
         [
@@ -66,20 +160,6 @@ return [
                     'user_role' => []
                 ]
             ]
-        ],
-        [
-            'user_role' => [],
-            'title' => 'Nhà Cung Cấp',
-            'icon' => 'fa fa-truck',
-            'route' => ['supplier.list'],
-            'subModule' => [
-                [
-                    'title' => 'Nhà cung cấp ',
-                    'route' => 'supplier.list',
-                    'icon' => 'fa fa-address-book',
-                    'user_role' => [] 
-                ],
-            ]
-        ],
+        ]
     ]
 ];
