ae06ea22db0e0a4c99ca4640308978246a5412b1
anhhoai <phamanhhoaipl@mail.com>
2024-09-03 22:59:07 +0700

    Trang Kiểm Kho

--- routes/admin/check_warehouse.php
+++ routes/admin/check_warehouse.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\Warehouse\CheckWarehouseController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::prefix('warehouse')->group(function () {
     Route::get('/index', [CheckWarehouseController::class, 'index'])->name('check_warehouse.index');
+    Route::get('/store', [CheckWarehouseController::class, 'create'])->name('check_warehouse.create');
+    Route::post('/store', [CheckWarehouseController::class, 'store'])->name('check_warehouse.store');
 });
+
--- routes/admin/warehouse.php
+++ routes/admin/warehouse.php
@@ -7,8 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('warehouse')->group(function () {
     Route::get('/import', [WarehouseController::class, 'import'])->name('warehouse.import');
     Route::get('/export', [WarehouseController::class, 'export'])->name('warehouse.export');
-    Route::get('/check', [WarehouseController::class, 'check'])->name('warehouse.check');
-
     Route::get('/create_import', [WarehouseController::class, 'create_import'])->name('warehouse.create_import');
     Route::get('/create_export', [WarehouseController::class, 'create_export'])->name('warehouse.create_export');
     Route::post('/store_export', [WarehouseController::class, 'store_export'])->name('warehouse.store_export');
