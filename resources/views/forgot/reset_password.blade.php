2c161941b9eb73e1a09ad4cc4b5cb8fbf19f4133
nhathuy <lamnhathuy0393418721@gmail.com>
2024-10-10 17:50:23 +0700

    hotfix: giao diện quên mật khẩu

--- resources/views/forgot/reset_password.blade.php
+++ resources/views/forgot/reset_password.blade.php
@@ -3,67 +3,76 @@
 
 <head>
     <meta charset="UTF-8">
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
     <title>@yield('title', 'Kho Y Tế Nhà Bee')</title>
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
     <link rel="stylesheet" href="{{ asset('css/fullcalendar.bundle.css') }}">
-
     <link rel="stylesheet" href="{{ asset('css/plugins.bundle.css') }}">
-
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
-
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
-
     <link rel="shortcut icon" href="{{ asset('image/logo_warehouse.png') }}" />
+
+    <style>
+        body,
+        input,
+        select,
+        textarea,
+        button {
+            font-family: 'Noto Sans', sans-serif;
+        }
+    </style>
 </head>
 
-<div class="d-flex flex-column flex-root">
-    <div
-        class="d-flex flex-column flex-column-fluid bgi-position-y-bottom position-x-center bgi-no-repeat bgi-size-contain bgi-attachment-fixed w-100">
-        <div class="d-flex flex-center flex-column flex-column-fluid p-10 pb-lg-20">
-            <a href="{{ route('home') }}" class="mb-7">
-                <img alt="Logo" src="{{ asset('image/logo_warehouse.png') }}" class="h-100px" />
-            </a>
-            <div class="w-lg-500px bg-body rounded shadow-sm p-10 p-lg-15 mx-auto">
-                <form method="POST" action="{{ route('home.reset_password') }}" class="form w-100"
-                    novalidate="novalidate" id="kt_password_reset_form">
-                    @csrf
-                    <div class="text-center mb-10">
-                        <h2 class="text-dark mb-3">Nhập mật khẩu mới</h2>
+<body>
+    <div class="d-flex flex-column flex-root">
+        <div
+            class="d-flex flex-column flex-column-fluid bgi-position-y-bottom position-x-center bgi-no-repeat bgi-size-contain bgi-attachment-fixed w-100">
+            <div class="d-flex flex-center flex-column flex-column-fluid p-10 pb-lg-20">
+                <div class="w-lg-500px bg-body rounded shadow-sm p-10 p-lg-15 mx-auto">
+                    <!-- Logo Container -->
+                    <div class="form-container text-center mb-7">
+                        <a href="{{ route('home') }}">
+                            <img alt="Logo" src="{{ asset('image/logo_warehouse.png') }}" class="h-100px mx-auto" />
+                        </a>
                     </div>
-                    <div class="fv-row mb-7">
-                        <label class="required form-label fw-bolder text-gray-900 fs-6">Mật khẩu mới</label>
-                        <input class="form-control form-control-sm border border-success" type="password"
-                            name="new_password" placeholder="Nhập mật khẩu mới..." />
-                        @error('new_password')
-                            <div class="message_error">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="fv-row mb-7">
-                        <label class="required form-label fw-bolder text-gray-900 fs-6">Xác nhận mật khẩu mới</label>
-                        <input class="form-control form-control-sm border border-success" type="password"
-                            name="new_password_confirmation" placeholder="Xác nhận mật khẩu mới..." />
-                        @error('new_password_confirmation')
-                            <div class="message_error">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="d-flex flex-wrap justify-content-center">
-                        <button type="submit" class="btn btn-dark btn-sm fw-bolder">
-                            <span>Thay đổi</span>
-                        </button>
-                    </div>
-                </form>
+                    <!-- Form -->
+                    <form method="POST" action="{{ route('home.reset_password') }}" class="form w-100"
+                        novalidate="novalidate" id="kt_password_reset_form">
+                        @csrf
+                        <div class="text-center mb-10">
+                            <h2 class="text-dark mb-3">Nhập mật khẩu mới</h2>
+                        </div>
+                        <div class="fv-row mb-7">
+                            <label class="required form-label fw-bolder text-gray-900 fs-6">Mật khẩu mới</label>
+                            <input class="form-control form-control-sm border border-success rounded-pill"
+                                type="password" name="new_password" placeholder="Nhập mật khẩu mới..." />
+                            @error('new_password')
+                                <div class="message_error">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="fv-row mb-7">
+                            <label class="required form-label fw-bolder text-gray-900 fs-6">Xác nhận mật khẩu
+                                mới</label>
+                            <input class="form-control form-control-sm border border-success rounded-pill"
+                                type="password" name="new_password_confirmation"
+                                placeholder="Xác nhận mật khẩu mới..." />
+                            @error('new_password_confirmation')
+                                <div class="message_error">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="d-flex flex-wrap justify-content-center">
+                            <button type="submit" class="btn btn-success btn-sm fw-bolder rounded-pill">
+                                <span>Thay đổi</span>
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
+</body>
 
 </html>
